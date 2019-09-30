ad116ba443ced2467a99f9302900ae6022560c75
fjiang6 <jiangfan6@outlook.com>
2019-09-30 17:21:55 +0800

    04koa 路由  get传值  动态路由

--- myapp/index.js
+++ myapp/index.js
@@ -1,6 +1,39 @@
 //引入Koa
 const koa = require('koa');
+const router = require('koa-router')();
 const app = new koa();
+
+/*配置路由*/
+router.get('/', function (ctx, next) {
+    ctx.body = "你好koa930";
+});
+
+router.get('/news', (ctx, next) => {
+    ctx.body = "新网 page930"
+});
+
+router.get('/newscontent',(ctx,next)=>{
+   let url = ctx.url;
+   /*从request 中获取GET请求*/
+   let request = ctx.request;
+   let req_query = request.query;
+   let req_querystring = request.querystring;
+   /*从上下文中直接获取*/
+    let ctx_query = ctx.query;
+    let ctx_querystring = ctx.querystring;
+    ctx.body={
+        url,
+        req_query,
+        req_querystring,
+        ctx_query,
+        ctx_querystring
+    }
+
+});
+
+app.use(router.routes()); //启动路由
+app.use(router.allowedMethods());
+
 
 //配置中间件
 app.use(async (ctx) => {
