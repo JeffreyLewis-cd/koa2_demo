b8db41b8bf183dedd8c72dd56fac4c8052bb1ef3
fjiang6 <jiangfan6@outlook.com>
2019-09-30 17:33:36 +0800

    04koa 路由 get传值 动态路由02

--- myapp/index.js
+++ myapp/index.js
@@ -12,24 +12,32 @@ router.get('/news', (ctx, next) => {
     ctx.body = "新网 page930"
 });
 
-router.get('/newscontent',(ctx,next)=>{
-   let url = ctx.url;
-   /*从request 中获取GET请求*/
-   let request = ctx.request;
-   let req_query = request.query;
-   let req_querystring = request.querystring;
-   /*从上下文中直接获取*/
+router.get('/newscontent', (ctx, next) => {
+    let url = ctx.url;
+    /*从request 中获取GET请求*/
+    let request = ctx.request;
+    let req_query = request.query;
+    let req_querystring = request.querystring;
+    /*从上下文中直接获取*/
     let ctx_query = ctx.query;
     let ctx_querystring = ctx.querystring;
-    ctx.body={
+    ctx.body = {
         url,
         req_query,
         req_querystring,
         ctx_query,
         ctx_querystring
-    }
+    };
+    console.log(ctx.body);
 
 });
+
+/*koa动态路由*/
+router.get("/product/:aid", async (ctx) => {
+    console.log(ctx.params);
+    ctx.body = '这是商品页面';
+});
+
 
 app.use(router.routes()); //启动路由
 app.use(router.allowedMethods());
